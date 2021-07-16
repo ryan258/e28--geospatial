0ad81d8061168185e264d57125d218d75a45d693
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-07-16 14:16:11 -0500

    Loaded the geospatial data in

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,19 +1,19 @@
 import { useEffect, useState } from 'react'
-import { csv } from 'd3'
+import { json } from 'd3'
+import { feature } from 'topojson'
 
-const csvUrl = 'https://gist.githubusercontent.com/curran/90240a6d88bdb1411467b21ea0769029/raw/7d4c3914cc6a29a7f5165f7d5d82b735d97bcfe4/week_temperature_sf.csv'
+const jsonUrl = 'https://unpkg.com/world-atlas@2.0.2/countries-50m.json'
 
 export const useData = () => {
   const [data, setData] = useState(null)
+  console.log(data)
 
   useEffect(() => {
-    const row = (d) => {
-      d.temperature = +d.temperature
-      d.timestamp = new Date(d.timestamp)
-      return d
-    }
-
-    csv(csvUrl, row).then(setData)
+    json(jsonUrl).then((topojsonData) => {
+      console.log(topojsonData)
+      const { countries } = topojsonData.objects
+      setData(feature(topojsonData, countries))
+    })
   }, [])
 
   return data
